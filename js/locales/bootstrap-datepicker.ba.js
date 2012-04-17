c4b368175720de21ed83defd5ea155af486438c2
Neven Falica <nfalica@element.hr>
2012-04-17 16:15:01 +0200

    Croatian, serbian and bosnian I18N package.

---
+++ js/locales/bootstrap-datepicker.ba.js
@@ -0,0 +1,13 @@
+/**
+ * Bosnian translation for bootstrap-datepicker
+ * Neven Falica <neven@falica.net>
+ */
+;(function($){
+  $.fn.datepicker.dates['hr'] = {
+    days: ["Nedjelja", "Ponedjeljak", "Utorak", "Srijeda", "Četvrtak", "Petak", "Subota", "Nedjelja"],
+    daysShort: ["Ned", "Pon", "Uto", "Sri", "Čet", "Pet", "Sub", "Ned"],
+    daysMin: ["N", "P", "U", "S", "Č", "P", "S", "N"],
+    months: ["Januar", "Februar", "Mart", "April", "Maj", "Jun", "Jul", "Avgust", "Septembar", "Oktobar", "Novembar", "Decembar"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Avg", "Sep", "Okt", "Nov", "Dec"]
+  };
+}(jQuery));
---
+++ js/locales/bootstrap-datepicker.si.js
@@ -0,0 +1,13 @@
+/**
+ * Slovenian translation for bootstrap-datepicker
+ * Neven Falica <neven@falica.net>
+ */
+;(function($){
+  $.fn.datepicker.dates['si'] = {
+    days: ["Nedelja", "Ponedeljek", "Torek", "Sreda", "Četrtek", "Petek", "Sobota", "Nedelja"],
+    daysShort: ["Ned", "Pon", "Tor", "Sre", "Čet", "Pet", "Sob", "Ned"],
+    daysMin: ["N", "P", "T", "S", "Č", "P", "S", "N"],
+    months: ["Januar", "Februar", "Marec", "April", "Maj", "Junij", "Julij", "Avgust", "September", "Oktober", "November", "December"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Avg", "Sep", "Okt", "Nov", "Dec"]
+  };
+}(jQuery));
